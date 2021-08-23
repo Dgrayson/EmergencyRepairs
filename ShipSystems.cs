085de33792085b1c1383999a55bc9d74b6753cc7
Dgrayson <dgrayson100@gmail.com>
2021-08-23 13:53:51 -0400

    Commiting leftover files befoore merge

--- ShipSystems.cs
+++ ShipSystems.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ShipSystems : MonoBehaviour
 {
+
+    public bool gravity;
+    public bool o2;
+    public bool navigation;
+
+    public float remainingO2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,16 @@ public class ShipSystems : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CalculateO2(); 
     }
+
+    private void CalculateO2()
+    {
+        if(!o2)
+        {
+            remainingO2 -= Time.deltaTime; 
+        }
+    }
+
+
 }
