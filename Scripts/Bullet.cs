7f84e0d7a8b524dc691461d627fe9a8be0ff0fd5
Dgrayson <dgrayson100@gmail.com>
2021-08-24 18:38:45 -0400

    Adding shoot files

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float timer = 5.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer <= 0)
+        {
+            Destroy(gameObject); 
+        }
+
+        timer -= Time.deltaTime; 
+    }
+}
