50b9956cad79eb3b33765bf2b89a5446b6129a8e
Dgrayson <dgrayson100@gmail.com>
2021-08-26 10:56:09 -0400

    Working on ammo UI

--- Scripts/PlayerShoot.cs
+++ Scripts/PlayerShoot.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerShoot : MonoBehaviour
 {
@@ -12,10 +14,17 @@ public class PlayerShoot : MonoBehaviour
     public float bulletSpeed;
     public float grenadeSpeed;
 
+    public int currbulletCount;
+    public int maxBulletCount; 
     public int grenadeCount; 
 
     public float currBulletTimer;
-    public float maxBulletTimer = .5f; 
+    public float maxBulletTimer = .5f;
+
+    public float reloadTime; 
+
+    public TextMeshProUGUI ammoCountUI;
+    public Image[] grenadeIcons; 
 
     void Start()
     {
@@ -25,7 +34,7 @@ public class PlayerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Mouse0))
+        if(Input.GetKey(KeyCode.Mouse0) && currbulletCount > 0)
         {
             if (currBulletTimer <= 0)
             {
@@ -39,6 +48,11 @@ public class PlayerShoot : MonoBehaviour
                 ThrowGrenade();
         }
 
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
+
         if(currBulletTimer > 0.0f)
             currBulletTimer -= Time.deltaTime; 
     }
@@ -47,7 +61,18 @@ public class PlayerShoot : MonoBehaviour
     {
         GameObject tempBullet = Instantiate(bullet, transform.position, Quaternion.identity);
 
-        tempBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed, ForceMode.Impulse); 
+        tempBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+
+        currbulletCount -= 1;
+        ammoCountUI.text = currbulletCount.ToString() + "/" + maxBulletCount.ToString();
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(reloadTime);
+
+        currbulletCount = maxBulletCount;
+        ammoCountUI.text = currbulletCount.ToString() + "/" + maxBulletCount.ToString(); 
     }
 
     void ThrowGrenade()
@@ -57,5 +82,7 @@ public class PlayerShoot : MonoBehaviour
         tempGrenade.GetComponent<Rigidbody>().AddForce(transform.forward * grenadeSpeed, ForceMode.VelocityChange);
 
         grenadeCount -= 1;
+
+        grenadeIcons[grenadeCount].enabled = false; 
     }
 }
